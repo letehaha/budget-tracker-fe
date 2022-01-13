7beb3596d329a48e58132960c4c750af9c0d342a
Dmitry Sviridenko <letehaha@gmail.com>
2022-01-14 01:06:51 +0200

    feat: finish jest config and add base test

--- jest.config.js
+++ jest.config.js
@@ -1,35 +1,30 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'json', 'vue'],
+  preset: 'ts-jest',
+  moduleFileExtensions: ['js', 'ts', 'json', 'vue'],
   transform: {
     // process *.vue files with vue-jest
-    '^.+\\.vue$': require.resolve('vue-jest'),
-    '.+\\.(css|styl|less|sass|scss|jpg|jpeg|png|svg|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-    require.resolve('jest-transform-stub'),
-    '^.+\\.jsx?$': require.resolve('babel-jest'),
-    '^.+\\.js?$': require.resolve('babel-jest'),
-    '^.+\\.md?$': require.resolve('markdown-loader-jest'),
+    '.+\\.(css|styl|less|sass|scss|jpg|jpeg|png|svg|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': require.resolve(
+      'jest-transform-stub',
+    ),
+    '^.+\\.vue?$': require.resolve('@vue/vue3-jest'),
   },
   transformIgnorePatterns: ['/node_modules/'],
-  // support the same @ -> src alias mapping in source code
   moduleNameMapper: {
-    '^@/(.*svg)(\\?inline)$': '<rootDir>/$1',
-    '^@/(.*)$': '<rootDir>/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^tests/(.*)$': '<rootDir>/tests/$1',
     '\\.(css|scss|less)$': 'identity-obj-proxy',
   },
-  // serializer for snapshots
-  snapshotSerializers: [
-    'jest-serializer-vue',
-  ],
+  moduleDirectories: ['node_modules', 'src'],
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
   modulePathIgnorePatterns: ['<rootDir>/cypress/'],
   // https://github.com/facebook/jest/issues/6766
   testURL: 'http://localhost/',
   testEnvironment: 'jsdom',
-  watchPlugins: [
-    require.resolve('jest-watch-typeahead/filename'),
-    require.resolve('jest-watch-typeahead/testname'),
-  ],
   globals: {
     NODE_ENV: 'test',
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+      isolatedModules: true,
+    },
   },
 };
---
+++ src/composable/form-validator.test.ts
@@ -0,0 +1,223 @@
+import { ref, computed } from 'vue';
+import { useFormValidation } from './form-validator';
+import { email } from '@/js/helpers/validators.helper';
+
+describe('useFormValidation', () => {
+  const validEmail = 'test@example.com';
+  const invalidEmail = 'foo';
+  describe('isFormValid', () => {
+    it('returns true for the whole form when form is ref and is valid', () => {
+      const form = ref({
+        email: validEmail,
+      });
+      const { isFormValid } = useFormValidation(
+        { form },
+        { form: { email: { email } } },
+      );
+
+      expect(isFormValid()).toBe(true);
+    });
+    it('returns true for the whole form when form is a raw object and is valid', () => {
+      const form = {
+        email: validEmail,
+      };
+      const { isFormValid } = useFormValidation(
+        { form },
+        { form: { email: { email } } },
+      );
+
+      expect(isFormValid()).toBe(true);
+    });
+    it('returns false if form is invalid', () => {
+      const form = {
+        email: invalidEmail,
+      };
+      const { isFormValid } = useFormValidation(
+        { form },
+        { form: { email: { email } } },
+      );
+
+      expect(isFormValid()).toBe(false);
+    });
+    it('returns true for the part of form if it is valid', () => {
+      const form = {
+        subform: {
+          email: validEmail,
+        },
+      };
+      const { isFormValid } = useFormValidation(
+        { form },
+        { form: { subform: { email: { email } } } },
+      );
+
+      expect(isFormValid('form.subform')).toBe(true);
+    });
+    it('returns false for the part of form if it is invalid', () => {
+      const form = {
+        subform: {
+          email: invalidEmail,
+        },
+      };
+      const { isFormValid } = useFormValidation(
+        { form },
+        { form: { subform: { email: { email } } } },
+      );
+
+      expect(isFormValid('form.subform')).toBe(false);
+    });
+    it('returns true if there is no validation rules', () => {
+      const form = {
+        email: invalidEmail,
+      };
+      const { isFormValid } = useFormValidation({ form }, {});
+
+      expect(isFormValid('form')).toBe(true);
+    });
+  });
+  describe('getFieldErrorMessage', () => {
+    it('returns nothing in case field is not invalid', () => {
+      const form = ref({
+        email: validEmail,
+      });
+      const { getFieldErrorMessage, touchField } = useFormValidation(
+        { form },
+        { form: { email: { email } } },
+      );
+
+      touchField('form.email');
+
+      expect(getFieldErrorMessage('form.email')).toBe('');
+    });
+    it('returns nothing in case form part is not invalid', () => {
+      const form = ref({
+        email: validEmail,
+      });
+      const { getFieldErrorMessage, touchField } = useFormValidation(
+        { form },
+        { form: { email: { email } } },
+      );
+
+      touchField('form.email');
+
+      expect(getFieldErrorMessage('form')).toBe('');
+    });
+    it('returns nothing in case field is not dirty', () => {
+      const form = ref({
+        email: invalidEmail,
+      });
+      const { getFieldErrorMessage } = useFormValidation(
+        { form },
+        { form: { email: { email } } },
+      );
+
+      expect(getFieldErrorMessage('form.email')).toBe('');
+    });
+    it('returns correct error message', () => {
+      const form = ref({
+        email: invalidEmail,
+      });
+      const { getFieldErrorMessage, touchField } = useFormValidation(
+        { form },
+        { form: { email: { email } } },
+      );
+
+      touchField('form.email');
+
+      expect(getFieldErrorMessage('form.email')).not.toBe('');
+    });
+    it('returns nothing in case error message is not specified', () => {
+      const form = ref({
+        email: invalidEmail,
+      });
+      const randomRule = 'randomRule';
+      const { getFieldErrorMessage, touchField } = useFormValidation(
+        { form },
+        { form: { email: { [randomRule]: email } } },
+      );
+
+      touchField('form.email');
+
+      expect(getFieldErrorMessage('form.email')).toBe('');
+    });
+    it('returns custom error message if it is passed and form is invalid', () => {
+      const form = ref({
+        email: invalidEmail,
+      });
+      const randomRule = 'randomRule';
+      const randomRuleMessage = 'This random field is invalid';
+      const customValidationMessages = {
+        [randomRule]: randomRuleMessage,
+      };
+      const { getFieldErrorMessage, touchField } = useFormValidation(
+        { form },
+        { form: { email: { [randomRule]: email } } },
+        undefined,
+        { customValidationMessages },
+      );
+
+      touchField('form.email');
+
+      expect(getFieldErrorMessage('form.email')).toBe(randomRuleMessage);
+    });
+    it('returns nothing when form is invalid and if custom error messages is passed but with wrong rule', () => {
+      const form = ref({
+        email: invalidEmail,
+      });
+      const randomRule = 'randomRule';
+      const randomRuleMessage = 'This random field is invalid';
+      const customValidationMessages = {
+        wrongRandomRule: randomRuleMessage,
+      };
+      const { getFieldErrorMessage, touchField } = useFormValidation(
+        { form },
+        { form: { email: { [randomRule]: email } } },
+        undefined,
+        { customValidationMessages },
+      );
+
+      touchField('form.email');
+
+      expect(getFieldErrorMessage('form.email')).toBe('');
+    });
+    it('returns custom error message which has Ref format', () => {
+      const form = ref({
+        email: invalidEmail,
+      });
+      const randomRule = 'randomRule';
+      const randomRuleMessage = 'This random field is invalid';
+      const customValidationMessages = {
+        [randomRule]: ref(randomRuleMessage),
+      };
+      const { getFieldErrorMessage, touchField } = useFormValidation(
+        { form },
+        { form: { email: { [randomRule]: email } } },
+        undefined,
+        { customValidationMessages },
+      );
+
+      touchField('form.email');
+
+      expect(getFieldErrorMessage('form.email')).toBe(randomRuleMessage);
+    });
+    it('returns custom error message which has ComputedRef format', () => {
+      const form = ref({
+        email: invalidEmail,
+      });
+      const randomRule = 'randomRule';
+      const randomRuleMessage = 'This random field is invalid';
+      const customValidationMessages = {
+        [randomRule]: computed(() => randomRuleMessage),
+      };
+      const { getFieldErrorMessage, touchField } = useFormValidation(
+        { form },
+        { form: { email: { [randomRule]: email } } },
+        undefined,
+        { customValidationMessages },
+      );
+
+      touchField('form.email');
+
+      expect(getFieldErrorMessage('form.email')).toBe(randomRuleMessage);
+    });
+  });
+});
