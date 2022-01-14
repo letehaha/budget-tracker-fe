9a0da9a9288d57b3ea6418c2448ae20bdb1cbc32
Dmitry Sviridenko <letehaha@gmail.com>
2022-01-14 02:53:48 +0200

    fix: babel config issue

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,10 @@ module.exports = {
     '@vue/cli-plugin-babel/preset',
   ],
   plugins: [
-    // to get rid of babel warning when running Storybook
+    // to get rid of babel warning when running Storybook and building the
+    // project
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
     ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
   ],
 };
