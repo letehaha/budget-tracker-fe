e6b4b5c4d40139f45ce90964be74f5e4a5d829ab
Dmitry Sviridenko <letehaha@gmail.com>
2022-01-14 02:46:56 +0200

    feat: add simple Storybook component

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,8 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset',
   ],
+  plugins: [
+    // to get rid of babel warning when running Storybook
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+  ],
 };
