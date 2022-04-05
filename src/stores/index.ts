3dd8cc4e854b9054e6692f40fa02ae868da1281e
Dmitry Sviridenko <letehaha@gmail.com>
2022-04-06 00:10:01 +0300

    fix: cycle deps warnings

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,10 +1,13 @@
+// TODO: fix deps cycle issues
 export { useCurrenciesStore } from './currencies';
+// eslint-disable-next-line
 export { useRootStore } from './root';
 export { useAccountTypesStore } from './account-types';
 export { usePaymentTypesStore } from './payment-types';
 export { useTransactionTypesStore } from './transaction-types';
 export { useAccountsStore } from './accounts';
 export { useUserStore } from './user';
+// eslint-disable-next-line
 export { useTransactionsStore } from './transactions';
 export { useCategoriesStore } from './categories/categories';
 export { useAuthStore } from './auth';
@@ -13,4 +16,5 @@ export { useAuthStore } from './auth';
 export { useCryptoBinanceStore } from './integrations/crypto/binance';
 
 // banks
+// eslint-disable-next-line
 export { useBanksMonobankStore } from './integrations/banks/monobank';
