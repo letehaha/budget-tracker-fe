234b6f99d53997a6e18950ed31f06110bf2859c9
Dmytro Svyrydenko <letehaha@gmail.com>
2022-05-01 11:56:53 +0300

    fix: system form amount formatting (#93)

--- src/js/helpers/formatters.helper.ts
+++ src/js/helpers/formatters.helper.ts
@@ -6,9 +6,16 @@ export const formatFiat = (value: unknown): string => Number(value).toFixed(2);
 /**
  * Converts passed value to the system one, since system does not have decimals
  *
- * @param {Number} value - any value
+ * @param {Number} value
  * @returns Correct system amount with no decimals
  */
 export const toSystemAmount = (value: number): number => (
   Number((value * 100).toFixed(0))
 );
+
+/**
+ *
+ * @param value
+ * @returns System value which might be displayed to the user
+ */
+export const fromSystemAmount = (value: number): number => value / 100;
