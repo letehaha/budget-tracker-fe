61db4420cbff9a5d4a5ee7915a1f367fe012c835
Dmitry Sviridenko <letehaha@gmail.com>
2022-06-06 00:48:26 +0300

    feat: add additional tests for SignIn form

--- cypress/e2e/SignIn.cy.ts
+++ cypress/e2e/SignIn.cy.ts
@@ -2,8 +2,45 @@ describe('SignIn.cy.ts', () => {
   it('should redirect here if user is not authorized', () => {
     cy.visit('/');
 
-    cy.location().should((loc) => {
-      expect(loc.pathname).to.eq('/sign-in');
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/sign-in');
     });
+  });
+
+  it('user should be able to sign in', () => {
+    cy.visit('/sign-in');
+
+    cy.get('input[name="username"]').type('letehaha');
+    cy.get('input[type="password"]').type('password');
+
+    cy.get('button[type="button"').click();
+
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/');
+    });
+  });
+
+  it('user cannot sign in with invalid credentials', () => {
+    cy.visit('/sign-in');
+
+    cy.get('input[name="username"]').type('invalid');
+    cy.get('input[type="password"]').type('invalid');
+
+    cy.get('button[type="button"]').click();
+
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/sign-in');
+    });
+  });
+
+  it('user should see error message with invalid credentials', () => {
+    cy.visit('/sign-in');
+
+    cy.get('input[name="username"]').type('invalid');
+    cy.get('input[type="password"]').type('invalid');
+
+    cy.get('button[type="button"]').click();
+
+    cy.get('.form-wrapper__error').should('be.visible');
   });
 });
