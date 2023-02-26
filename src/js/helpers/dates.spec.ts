7ff723d481ac29ada5c4a2d46c3f2bf2cef27470
Dmitry Sviridenko <letehaha@gmail.com>
2023-02-26 19:05:53 +0200

    chore: Improve getHoursInMilliseconds tests

--- src/js/helpers/dates.spec.ts
+++ src/js/helpers/dates.spec.ts
@@ -1,11 +1,18 @@
 import { getHoursInMilliseconds } from './dates';
 
 describe('js/helpers/dates', () => {
-  test.each([
-    [10, 36_000_000],
-    [1.5, 5_400_000],
-    [0.1, 360_000],
-  ])('%s hours is %s millliseconds', (value, expected) => {
-    expect(getHoursInMilliseconds(value)).toBe(expected);
+  describe('getHoursInMilliseconds', () => {
+    test.each([
+      [10, 36_000_000],
+      [1.5, 5_400_000],
+      [0.1, 360_000],
+      [0, 0],
+      [-10, -36_000_000],
+      [Infinity, Infinity],
+      [-Infinity, -Infinity],
+      [NaN, NaN],
+    ])('%s hours is %s millliseconds', (value, expected) => {
+      expect(getHoursInMilliseconds(value)).toBe(expected);
+    });
   });
 });
