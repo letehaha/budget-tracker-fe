0f6538eaaa62ab74840b026a9d7dc9ba67630c34
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2023-08-19 16:48:37 +0300

    feat: Show chart data only for current month

--- src/components/widgets/current-balance.vue
+++ src/components/widgets/current-balance.vue
@@ -3,7 +3,11 @@
     <h3 class="current-balance-widget__title">
       Balance trend
     </h3>
-    <highcharts class="current-balance-widget__chart" :options="chartOptions" />
+    <highcharts
+      v-node-resize-observer="{ callback: onChartResize }"
+      class="current-balance-widget__chart"
+      :options="chartOptions"
+    />
   </div>
 </template>
 
@@ -12,23 +16,41 @@ import {
   defineComponent, onMounted, ref, computed,
 } from 'vue';
 import { Chart } from 'highcharts-vue';
-import { format, subDays } from 'date-fns';
+import {
+  format, subDays, getDaysInMonth, addDays, startOfMonth, endOfMonth, startOfDay,
+} from 'date-fns';
 import { storeToRefs } from 'pinia';
 import { getBalanceHistory } from '@/api';
 import { fromSystemAmount, toLocalFiatCurrency } from '@/js/helpers';
 import { useCurrenciesStore } from '@/stores';
 import { aggregateData } from './helpers';
 
+const currentDayInMonth = new Date().getDate();
+
 const loadBalanceData = async () => {
-  const loadDataForDays = 30;
   const result = await getBalanceHistory({
-    from: subDays(new Date(), loadDataForDays),
+    from: subDays(new Date(), currentDayInMonth - 1),
   });
 
   if (!result?.length) return [];
 
   return aggregateData(result);
 };
+
+// Calculate it manually so shart will always have first and last ticks (dates)
+function generateDateSteps(datesToShow = 5) {
+  const step = Math.round(getDaysInMonth(new Date()) / datesToShow);
+  const start = startOfMonth(new Date()).getTime();
+  const end = startOfDay(endOfMonth(new Date())).getTime();
+
+  const dates = [start];
+  while (dates[dates.length - 1] < end) {
+    const nextDate = addDays(dates[dates.length - 1], step).getTime();
+    dates.push(nextDate <= end ? nextDate : end);
+  }
+
+  return dates;
+}
 
 export default defineComponent({
   name: 'current-balance-widget',
@@ -38,104 +60,132 @@ export default defineComponent({
   setup() {
     const currenciesStore = useCurrenciesStore();
     const { baseCurrency } = storeToRefs(currenciesStore);
-    const balanceHistory = ref([]);
-    const chartOptions = computed(() => ({
-      chart: {
-        type: 'area',
-        backgroundColor: 'transparent',
-        height: 270,
-      },
-      title: null,
-      xAxis: {
-        type: 'datetime',
-        dateTimeLabelFormats: {
-          day: '%d %b',
+    const balanceHistory = ref<{ amount: number; date: string }[]>([]);
+    const currentChartWidth = ref(null);
+    const chartOptions = computed(() => {
+      const pixelsPerTick = 120;
+      const ticksAmount = currentChartWidth.value
+        ? Math.round(currentChartWidth.value / pixelsPerTick)
+        : 5;
+
+      return {
+        chart: {
+          type: 'area',
+          backgroundColor: 'transparent',
+          height: 270,
         },
-        // Show fullheight crosshair for the selected point
-        // crosshair: true,
-        gridLineWidth: 0,
-        labels: {
-          style: {
-            color: 'var(--app-text-base)',
-          },
+        // So xAxis will be rendered correctly but not as hours
+        time: {
+          useUTC: false,
         },
-      },
-      yAxis: {
         title: null,
-        labels: {
-          style: {
-            color: 'var(--app-text-base)',
+        xAxis: {
+          type: 'datetime',
+          dateTimeLabelFormats: {
+            day: '%d %b',
+          },
+          // Show fullheight crosshair for the selected point
+          // crosshair: true
+          tickPositions: generateDateSteps(ticksAmount),
+          gridLineWidth: 0,
+          labels: {
+            style: {
+              color: 'var(--app-text-base)',
+            },
           },
         },
-        gridLineColor: 'rgba(var(--app-primary-rgb), 0.1)',
-      },
-      plotOptions: {
-        area: {
-          fillOpacity: 0.5,
-          lineColor: 'var(--app-primary)',
-          lineWidth: 2,
-          states: {
-            hover: {
-              lineWidth: 3,
+        yAxis: {
+          title: null,
+          labels: {
+            style: {
+              color: 'var(--app-text-base)',
             },
           },
-          threshold: null,
-          fillColor: {
-            linearGradient: {
-              x1: 0, x2: 0, y1: 0, y2: 1,
-            },
-            stops: [
-              [0, 'rgba(var(--app-primary-rgb), 0.3)'],
-              [1, 'rgba(var(--app-primary-rgb), 0)'],
-            ],
-          },
-          marker: {
-            // Disable markers so the line will be smoother
-            enabled: false,
+          gridLineColor: 'rgba(var(--app-primary-rgb), 0.1)',
+        },
+        plotOptions: {
+          area: {
+            fillOpacity: 0.5,
+            lineColor: 'var(--app-primary)',
+            lineWidth: 2,
             states: {
               hover: {
-                enabled: true,
-                fillColor: 'var(--app-primary)',
-                lineColor: 'var(--app-primary)',
-                lineWidth: 0,
+                lineWidth: 3,
+              },
+            },
+            threshold: null,
+            fillColor: {
+              linearGradient: {
+                x1: 0, x2: 0, y1: 0, y2: 1,
+              },
+              stops: [
+                [0, 'rgba(var(--app-primary-rgb), 0.3)'],
+                [1, 'rgba(var(--app-primary-rgb), 0)'],
+              ],
+            },
+            marker: {
+              // Disable markers so the line will be smoother
+              enabled: false,
+              states: {
+                hover: {
+                  enabled: true,
+                  fillColor: 'var(--app-primary)',
+                  lineColor: 'var(--app-primary)',
+                  lineWidth: 0,
+                },
               },
             },
           },
         },
-      },
-      tooltip: {
-        useHTML: true,
-        backgroundColor: 'var(--app-bg-box)',
-        borderColor: 'transparent',
-        formatter() {
-          return `
-            <div class="current-balance-widget__tooltip">
-              <div class="current-balance-widget__tooltip-date">
-                ${format(this.x, 'MMMM d, yyyy')}
+        tooltip: {
+          useHTML: true,
+          backgroundColor: 'var(--app-bg-box)',
+          borderColor: 'transparent',
+          formatter() {
+            return `
+              <div class="current-balance-widget__tooltip">
+                <div class="current-balance-widget__tooltip-date">
+                  ${format(this.x, 'MMMM d, yyyy')}
+                </div>
+                <div class="current-balance-widget__tooltip-value">
+                  Balance: <span>${toLocalFiatCurrency(this.y, { currency: baseCurrency.value.currency.code })}</span>
+                </div>
               </div>
-              <div class="current-balance-widget__tooltip-value">
-                Balance: <span>${toLocalFiatCurrency(this.y, { currency: baseCurrency.value.currency.code })}</span>
-              </div>
-            </div>
-          `;
+            `;
+          },
+          shadow: false,
+          borderRadius: 8,
+          style: {
+            color: 'var(--app-text-base)',
+          },
         },
-        shadow: false,
-        borderRadius: 8,
-        style: {
-          color: 'var(--app-text-base)',
-        },
-      },
-      series: [
-        {
-          showInLegend: false,
-          data: balanceHistory.value.map((point) => [
-            new Date(point.date).getTime(),
-            fromSystemAmount(point.amount),
-          ]),
-        },
-      ],
-      credits: false,
-    }));
+        series: [
+          {
+            showInLegend: false,
+            data: [
+              ...balanceHistory.value.map((point) => [
+                new Date(point.date).getTime(),
+                fromSystemAmount(point.amount),
+              ]),
+              // fill remaining days with `null` so chart will be rendered for all
+              // days in the month
+              ...Array(getDaysInMonth(new Date()) - currentDayInMonth)
+                .fill([])
+                .map((_, i) => [
+                  addDays(new Date(), i + 1).getTime(),
+                  null,
+                ]),
+            ],
+          },
+        ],
+        credits: false,
+      };
+    });
+
+    const onChartResize = (entries: ResizeObserverEntry[]) => {
+      const entry = entries[0];
+      currentChartWidth.value = entry.contentRect.width;
+    };
 
     onMounted(async () => {
       balanceHistory.value = await loadBalanceData();
@@ -145,6 +195,7 @@ export default defineComponent({
       balanceHistory,
       chartOptions,
       baseCurrency,
+      onChartResize,
     };
   },
 });
