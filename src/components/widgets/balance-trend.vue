55c8ede686bab799fe2e348fac28c198a87a3020
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2023-08-22 01:34:36 +0300

    feat: Move area chart to sharable composable

--- src/components/widgets/balance-trend.vue
+++ src/components/widgets/balance-trend.vue
@@ -35,20 +35,16 @@
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
+import { onMounted, ref, computed } from 'vue';
+import { Chart as Highcharts } from 'highcharts-vue';
 import {
-  defineComponent, onMounted, ref, computed,
-} from 'vue';
-import { Chart } from 'highcharts-vue';
-import {
-  format, subDays, getDaysInMonth, addDays,
+  subDays, getDaysInMonth, addDays,
   startOfMonth, endOfMonth, startOfDay, subMonths,
 } from 'date-fns';
-import { storeToRefs } from 'pinia';
 import { getBalanceHistory, getTotalBalance } from '@/api';
-import { fromSystemAmount, toLocalFiatCurrency, calculatePercentageDifference } from '@/js/helpers';
-import { useCurrenciesStore } from '@/stores';
-import { useFormatCurrency } from '@/composable';
+import { calculatePercentageDifference } from '@/js/helpers';
+import { useFormatCurrency, useHighcharts } from '@/composable';
 
 import { aggregateData } from './helpers';
 
@@ -77,174 +73,71 @@ function generateDateSteps(datesToShow = 5) {
   return dates;
 }
 
-export default defineComponent({
+defineOptions({
   name: 'balance-trend-widget',
-  components: {
-    Highcharts: Chart,
-  },
-  setup() {
-    const currenciesStore = useCurrenciesStore();
-    const { baseCurrency } = storeToRefs(currenciesStore);
-    const balanceHistory = ref<{ amount: number; date: string }[]>([]);
-    const currentChartWidth = ref(null);
-    const { formatBaseCurrency } = useFormatCurrency();
-    const chartOptions = computed(() => {
-      const pixelsPerTick = 120;
-      const ticksAmount = currentChartWidth.value
-        ? Math.round(currentChartWidth.value / pixelsPerTick)
-        : 5;
+});
 
-      return {
-        chart: {
-          type: 'area',
-          backgroundColor: 'transparent',
-          height: 220,
-        },
-        // So xAxis will be rendered correctly but not as hours
-        time: {
-          useUTC: false,
-        },
-        title: null,
-        xAxis: {
-          type: 'datetime',
-          dateTimeLabelFormats: {
-            day: '%d %b',
-          },
-          // Show fullheight crosshair for the selected point
-          // crosshair: true
-          tickPositions: generateDateSteps(ticksAmount),
-          gridLineWidth: 0,
-          labels: {
-            style: {
-              color: 'var(--app-text-base)',
-            },
-          },
-        },
-        yAxis: {
-          title: null,
-          labels: {
-            style: {
-              color: 'var(--app-text-base)',
-            },
-          },
-          gridLineColor: 'rgba(var(--app-primary-rgb), 0.1)',
-        },
-        plotOptions: {
-          area: {
-            fillOpacity: 0.5,
-            lineColor: 'var(--app-primary)',
-            lineWidth: 2,
-            states: {
-              hover: {
-                lineWidth: 3,
-              },
-            },
-            threshold: null,
-            fillColor: {
-              linearGradient: {
-                x1: 0, x2: 0, y1: 0, y2: 1,
-              },
-              stops: [
-                [0, 'rgba(var(--app-primary-rgb), 0.3)'],
-                [1, 'rgba(var(--app-primary-rgb), 0)'],
-              ],
-            },
-            marker: {
-              // Disable markers so the line will be smoother
-              enabled: false,
-              states: {
-                hover: {
-                  enabled: true,
-                  fillColor: 'var(--app-primary)',
-                  lineColor: 'var(--app-primary)',
-                  lineWidth: 0,
-                },
-              },
-            },
-          },
-        },
-        tooltip: {
-          useHTML: true,
-          backgroundColor: 'var(--app-bg-box)',
-          borderColor: 'transparent',
-          formatter() {
-            return `
-              <div class="balance-trend-widget__tooltip">
-                <div class="balance-trend-widget__tooltip-date">
-                  ${format(this.x, 'MMMM d, yyyy')}
-                </div>
-                <div class="balance-trend-widget__tooltip-value">
-                  Balance: <span>${toLocalFiatCurrency(this.y, { currency: baseCurrency.value.currency.code })}</span>
-                </div>
-              </div>
-            `;
-          },
-          shadow: false,
-          borderRadius: 8,
-          style: {
-            color: 'var(--app-text-base)',
-          },
-        },
-        series: [
-          {
-            showInLegend: false,
-            data: [
-              ...balanceHistory.value.map((point) => [
-                new Date(point.date).getTime(),
-                fromSystemAmount(point.amount),
-              ]),
-              // fill remaining days with `null` so chart will be rendered for all
-              // days in the month
-              ...Array(getDaysInMonth(new Date()) - currentDayInMonth)
-                .fill([])
-                .map((_, i) => [
-                  addDays(new Date(), i + 1).getTime(),
-                  null,
-                ]),
-            ],
-          },
+const balanceHistory = ref<{ amount: number; date: string }[]>([]);
+const currentChartWidth = ref(null);
+const { formatBaseCurrency } = useFormatCurrency();
+const { buildAreaChartConfig } = useHighcharts();
+const chartOptions = computed(() => {
+  const pixelsPerTick = 120;
+  const ticksAmount = currentChartWidth.value
+    ? Math.round(currentChartWidth.value / pixelsPerTick)
+    : 5;
+
+  return buildAreaChartConfig({
+    chart: { height: 220 },
+    xAxis: {
+      tickPositions: generateDateSteps(ticksAmount),
+    },
+    series: [
+      {
+        showInLegend: false,
+        data: [
+          ...balanceHistory.value.map((point) => [
+            new Date(point.date).getTime(),
+            point.amount,
+          ]),
+          // fill remaining days with `null` so chart will be rendered for all
+          // days in the month
+          ...Array(getDaysInMonth(new Date()) - currentDayInMonth)
+            .fill([])
+            .map((_, i) => [
+              addDays(new Date(), i + 1).getTime(),
+              null,
+            ]),
         ],
-        credits: false,
-      };
-    });
-    const todayBalance = ref(null);
-    const previousBalance = ref(null);
-    const balancesDiff = computed<number>(() => {
-      if (!todayBalance.value || !previousBalance.value) return 0;
+      },
+    ],
+  });
+});
+const todayBalance = ref(null);
+const previousBalance = ref(null);
+const balancesDiff = computed<number>(() => {
+  if (!todayBalance.value || !previousBalance.value) return 0;
 
-      const percentage = Number(calculatePercentageDifference(
-        todayBalance.value,
-        previousBalance.value,
-      )).toFixed(2);
-      return Number(percentage);
-    });
+  const percentage = Number(calculatePercentageDifference(
+    todayBalance.value,
+    previousBalance.value,
+  )).toFixed(2);
+  return Number(percentage);
+});
 
-    const onChartResize = (entries: ResizeObserverEntry[]) => {
-      const entry = entries[0];
-      currentChartWidth.value = entry.contentRect.width;
-    };
+const onChartResize = (entries: ResizeObserverEntry[]) => {
+  const entry = entries[0];
+  currentChartWidth.value = entry.contentRect.width;
+};
 
-    onMounted(async () => {
-      balanceHistory.value = await loadBalanceData({
-        from: subDays(new Date(), currentDayInMonth - 1),
-      });
-      todayBalance.value = await getTotalBalance({ date: new Date() });
-      previousBalance.value = await getTotalBalance({
-        date: subMonths(new Date(), 1),
-      });
-    });
-
-    return {
-      balanceHistory,
-      formatBaseCurrency,
-      chartOptions,
-      baseCurrency,
-      todayBalance,
-      previousBalance,
-      balancesDiff,
-      onChartResize,
-    };
-  },
+onMounted(async () => {
+  balanceHistory.value = await loadBalanceData({
+    from: subDays(new Date(), currentDayInMonth - 1),
+  });
+  todayBalance.value = await getTotalBalance({ date: new Date() });
+  previousBalance.value = await getTotalBalance({
+    date: subMonths(new Date(), 1),
+  });
 });
 </script>
 
