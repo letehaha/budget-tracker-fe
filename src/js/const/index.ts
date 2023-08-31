068767acb9436f515f5c6aa536e1ed2caec568ca
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2023-08-31 21:29:21 +0300

    chore: Delete dead code

--- src/js/const/index.ts
+++ src/js/const/index.ts
@@ -1,7 +1,3 @@
-export { PAGE_TRANSITIONS } from './page-transitions.const';
-export { TRANSACTION_TYPES } from './transaction.const';
-export { API_ERROR_CODES } from './error-codes.const';
-
 export const MONOBANK_API_TOKEN_LENGTH = 44;
 
 export const isDevEnv = process.env.NODE_ENV === 'development';
--- src/js/const/transaction.const.ts
+++
@@ -1,4 +0,0 @@
-export const TRANSACTION_TYPES = Object.freeze({
-  system: 1,
-  monobank: 2,
-});
