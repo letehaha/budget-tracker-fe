8f79ff3ee7a306b95300f65f74cda3cd25b3a50e
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2024-01-07 15:33:43 +0200

    feat: Add basic unit tests for modify-record helpers

---
+++ tests/mocks/transactions.ts
@@ -0,0 +1,48 @@
+import {
+  TRANSACTION_TYPES, TRANSACTION_TRANSFER_NATURE, TransactionModel, PAYMENT_TYPES, ACCOUNT_TYPES,
+} from 'shared-types';
+
+const SHARED_TX_BODY: TransactionModel = {
+  id: 1274,
+  amount: 9,
+  refAmount: 322.65,
+  note: null,
+  time: new Date(),
+  userId: 40,
+  transactionType: TRANSACTION_TYPES.income,
+  paymentType: PAYMENT_TYPES.creditCard,
+  accountId: 127,
+  categoryId: 3121,
+  currencyId: 769,
+  currencyCode: 'EUR',
+  accountType: ACCOUNT_TYPES.system,
+  refCurrencyCode: 'UAH',
+  transferNature: TRANSACTION_TRANSFER_NATURE.not_transfer,
+  transferId: null,
+  originalId: null,
+  externalData: null,
+  commissionRate: 0,
+  refCommissionRate: 0,
+  cashbackAmount: 0,
+};
+
+export const INCOME_TRANSACTION: TransactionModel = {
+  ...SHARED_TX_BODY,
+  transactionType: TRANSACTION_TYPES.income,
+  transferNature: TRANSACTION_TRANSFER_NATURE.not_transfer,
+};
+export const EXPENSE_TRANSACTION: TransactionModel = {
+  ...SHARED_TX_BODY,
+  transactionType: TRANSACTION_TYPES.expense,
+  transferNature: TRANSACTION_TRANSFER_NATURE.not_transfer,
+};
+export const COMMON_TRANSFER_TRANSACTION: TransactionModel = {
+  ...SHARED_TX_BODY,
+  transactionType: TRANSACTION_TYPES.expense,
+  transferNature: TRANSACTION_TRANSFER_NATURE.common_transfer,
+};
+export const OUT_OF_WALLET_TRANSACTION: TransactionModel = {
+  ...SHARED_TX_BODY,
+  transactionType: TRANSACTION_TYPES.expense,
+  transferNature: TRANSACTION_TRANSFER_NATURE.transfer_out_wallet,
+};
