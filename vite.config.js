a9f0c70bd008c2b83ed55c46d1e85ec9dcc0cf29
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2024-01-23 22:53:31 +0200

    feat: Add global configuration + tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import svgLoader from "vite-svg-loader";
 
+import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd(), "") };
@@ -36,6 +39,9 @@ export default ({ mode }) => {
       watch: false,
     },
     css: {
+      postcss: {
+        plugins: [tailwind(), autoprefixer()],
+      },
       preprocessorOptions: {
         scss: {
           additionalData: '@import "./src/styles/resources/index.scss";',
