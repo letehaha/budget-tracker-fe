0f51d7b2460c4ace956f9d5e2a9afb88a5a6fef9
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2024-01-28 14:19:09 +0200

    feat: Show account category on details page

---
+++ src/common/const/account-categories-verbose.ts
@@ -0,0 +1,16 @@
+import { ACCOUNT_CATEGORIES } from "shared-types";
+
+export const ACCOUNT_CATEGORIES_VERBOSE = Object.freeze({
+  [ACCOUNT_CATEGORIES.general]: "General",
+  [ACCOUNT_CATEGORIES.cash]: "Cash",
+  [ACCOUNT_CATEGORIES.currentAccount]: "Current account",
+  [ACCOUNT_CATEGORIES.creditCard]: "Credit card",
+  [ACCOUNT_CATEGORIES.saving]: "Saving account",
+  [ACCOUNT_CATEGORIES.bonus]: "Bonus",
+  [ACCOUNT_CATEGORIES.insurance]: "Insurance",
+  [ACCOUNT_CATEGORIES.investment]: "Investment",
+  [ACCOUNT_CATEGORIES.loan]: "Loan",
+  [ACCOUNT_CATEGORIES.mortgage]: "Mortgage",
+  [ACCOUNT_CATEGORIES.overdraft]: "Overdraft",
+  [ACCOUNT_CATEGORIES.crypto]: "Crypto",
+});
