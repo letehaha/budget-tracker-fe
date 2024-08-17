e57f2d6d6d3dbd597a5d70cd55643a3792a71857
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2024-08-17 11:02:56 +0200

    feat: Re-load currencies when pairing external accounts

--- src/components/modals/monobank-set-token.vue
+++ src/components/modals/monobank-set-token.vue
@@ -1,22 +1,27 @@
 <template>
-  <div class="monobank-set-token" data-cy="monobank-set-token-modal">
-    <ui-button
+  <Card
+    class="py-12 px-8 w-full max-w-[600px] relative rounded-xl"
+    data-cy="monobank-set-token-modal"
+  >
+    <Button
       type="button"
-      class="button-style-reset monobank-set-token__close"
+      class="absolute top-3 right-3"
       theme="light-dark"
       is-icon
       @click="$emit(MODAL_EVENTS.closeModal)"
     >
       X
-    </ui-button>
+    </Button>
 
-    <p class="monobank-set-token__descr">
+    <p class="my-5">
       Please visit
-      <a href="https://api.monobank.ua/">https://api.monobank.ua/</a>
+      <a href="https://api.monobank.ua/" class="text-primary"
+        >https://api.monobank.ua/</a
+      >
       and follow all the instructions. Paste the API token from Monobank in the
       field below
     </p>
-    <div class="monobank-set-token__row">
+    <div class="my-5">
       <input-field
         v-model="form.token"
         name="token"
@@ -24,24 +29,19 @@
         :error-message="getFieldErrorMessage('form.token')"
       />
     </div>
-    <div class="monobank-set-token__actions">
-      <ui-button
-        class="monobank-set-token__action monobank-set-token__action--submit"
-        type="submit"
-        :disabled="isLoading"
-        @click="submit"
-      >
+    <div class="flex justify-center">
+      <Button type="submit" :disabled="isLoading" @click="submit">
         <template v-if="isUpdate"> Update token </template>
         <template v-else> Pair account </template>
-      </ui-button>
+      </Button>
     </div>
-  </div>
+  </Card>
 </template>
 
-<script lang="ts">
-import { defineComponent, reactive, ref } from "vue";
+<script setup lang="ts">
+import { reactive, ref } from "vue";
 import { API_ERROR_CODES } from "shared-types";
-import { useBanksMonobankStore } from "@/stores";
+import { useBanksMonobankStore, useCurrenciesStore } from "@/stores";
 import { MONOBANK_API_TOKEN_LENGTH } from "@/common/const";
 import { ApiErrorResponseError } from "@/js/errors";
 import { useFormValidation } from "@/composable";
@@ -49,122 +49,89 @@ import { required, minLength } from "@/js/helpers/validators";
 import InputField from "@/components/fields/input-field.vue";
 import Button from "@/components/common/ui-button.vue";
 import { EVENTS as MODAL_EVENTS } from "@/components/modal-center/ui-modal.vue";
+import { Card } from "@/components/lib/ui/card";
 
 import {
   useNotificationCenter,
   NotificationType,
 } from "@/components/notification-center";
 
-export default defineComponent({
+defineOptions({
   name: "monobank-set-token",
-  components: {
-    "input-field": InputField,
-    "ui-button": Button,
+});
+
+const props = withDefaults(
+  defineProps<{
+    isUpdate?: boolean;
+  }>(),
+  {
+    isUpdate: false,
   },
-  props: {
-    isUpdate: { type: Boolean, default: false },
+);
+
+const emit = defineEmits([MODAL_EVENTS.closeModal]);
+const monobankStore = useBanksMonobankStore();
+const currenciesStore = useCurrenciesStore();
+const { addNotification } = useNotificationCenter();
+
+const isLoading = ref(false);
+const form = reactive({ token: "" });
+const { isFormValid, getFieldErrorMessage } = useFormValidation(
+  { form },
+  {
+    form: {
+      token: {
+        required,
+        apiToken: minLength(MONOBANK_API_TOKEN_LENGTH),
+      },
+    },
   },
-  emits: [MODAL_EVENTS.closeModal],
-  setup(props, { emit }) {
-    const monobankStore = useBanksMonobankStore();
-    const { addNotification } = useNotificationCenter();
+  undefined,
+  {
+    customValidationMessages: {
+      apiToken: `Monobank API token should be ${MONOBANK_API_TOKEN_LENGTH} characters length`,
+    },
+  },
+);
 
-    const isLoading = ref(false);
-    const form = reactive({ token: "" });
-    const { isFormValid, getFieldErrorMessage } = useFormValidation(
-      { form },
-      {
-        form: {
-          token: {
-            required,
-            apiToken: minLength(MONOBANK_API_TOKEN_LENGTH),
-          },
-        },
-      },
-      undefined,
-      {
-        customValidationMessages: {
-          apiToken: `Monobank API token should be ${MONOBANK_API_TOKEN_LENGTH} characters length`,
-        },
-      },
-    );
+const submit = async () => {
+  try {
+    if (!isFormValid()) return;
 
-    const submit = async () => {
-      try {
-        if (!isFormValid()) return;
+    isLoading.value = true;
 
-        isLoading.value = true;
+    if (props.isUpdate) {
+      await monobankStore.updateUser({ token: form.token });
+    } else {
+      await monobankStore.pairAccount({ token: form.token });
+    }
 
-        if (props.isUpdate) {
-          await monobankStore.updateUser({ token: form.token });
-        } else {
-          await monobankStore.pairAccount({ token: form.token });
-        }
+    await currenciesStore.loadCurrencies();
 
-        emit(MODAL_EVENTS.closeModal);
+    emit(MODAL_EVENTS.closeModal);
 
+    addNotification({
+      text: "Paired",
+      type: NotificationType.success,
+    });
+  } catch (e) {
+    if (e instanceof ApiErrorResponseError) {
+      if (e.data.code === API_ERROR_CODES.monobankUserAlreadyConnected) {
         addNotification({
-          text: "Paired",
-          type: NotificationType.success,
-        });
-      } catch (e) {
-        if (e instanceof ApiErrorResponseError) {
-          if (e.data.code === API_ERROR_CODES.monobankUserAlreadyConnected) {
-            addNotification({
-              text: "Account already connected",
-              type: NotificationType.error,
-            });
-
-            return;
-          }
-        }
-
-        addNotification({
-          text: "Unexpected error",
+          text: "Account already connected",
           type: NotificationType.error,
         });
-      } finally {
-        isLoading.value = false;
+
+        return;
       }
-    };
+    }
 
-    return {
-      MODAL_EVENTS,
-      form,
-      isLoading,
-      submit,
-      getFieldErrorMessage,
-    };
-  },
-});
-</script>
-
-<style lang="scss" scoped>
-.monobank-set-token {
-  background-color: var(--app-bg-box);
-  padding: 48px 32px;
-  width: 100%;
-  max-width: 600px;
-  position: relative;
-  border-radius: 12px;
-}
-.monobank-set-token__close {
-  position: absolute;
-  top: 12px;
-  right: 12px;
-}
-.monobank-set-token__row {
-  margin-bottom: 20px;
-}
-.monobank-set-token__actions {
-  display: flex;
-  justify-content: center;
-}
-.monobank-set-token__descr {
-  margin: 20px 0;
-
-  a {
-    color: var(--app-primary);
+    addNotification({
+      text: "Unexpected error",
+      type: NotificationType.error,
+    });
+  } finally {
+    isLoading.value = false;
   }
-}
-</style>
+};
+</script>
