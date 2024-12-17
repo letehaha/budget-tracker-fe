1d259a71fe639f239bdc59b7d845c3d7b3b07ae1
Dany <lysakdanil@gmail.com>
2024-12-17 15:35:42 +0200

    fix: change create account form card with

--- src/pages/account/create.vue
+++ src/pages/account/create.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-6">
-    <Card class="sticky h-min w-[380px] top-[var(--header-height)] p-4">
+    <Card class="sticky h-min w-[440px] top-[var(--header-height)] p-4">
       <CreateAccountForm class="max-w-[440px]" @created="pushToAccountsPage" />
     </Card>
   </div>
