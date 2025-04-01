9670067089914e407985f742065d6cccad3b7557
Dany <lysakdanil@gmail.com>
2025-04-01 08:09:47 +0300

    Feat: add budget page

---
+++ src/pages/budgets/budgets.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+import Button from "@/components/lib/ui/button/Button.vue";
+import Card from "@/components/lib/ui/card/Card.vue";
+import ResponsiveDialog from "@/components/common/responsive-dialog.vue";
+
+import CardHeader from "@/components/lib/ui/card/CardHeader.vue";
+import CardContent from "@/components/lib/ui/card/CardContent.vue";
+import BudgetCreation from "./budget-creation.vue";
+
+import BudgetList from "./budget-list.vue";
+
+const isOpen = ref(false);
+
+const openModal = () => {
+  isOpen.value = true;
+};
+</script>
+
+<template>
+  <div class="p-4">
+    <Card class="@container/budgets-card max-w-[700px]">
+      <CardHeader
+        :class="[
+          'border-b flex justify-between gap-4',
+          '@[450px]/budgets-card:flex-row @[450px]/budgets-card:items-center',
+        ]"
+      >
+        <h3 class="text-xl">Budgets list</h3>
+
+        <Button class="w-min" @click="openModal">Create budget +</Button>
+      </CardHeader>
+      <CardContent>
+        <BudgetList />
+      </CardContent>
+    </Card>
+
+    <ResponsiveDialog v-model:open="isOpen">
+      <template #title> Create budget </template>
+      <BudgetCreation />
+    </ResponsiveDialog>
+  </div>
+</template>
