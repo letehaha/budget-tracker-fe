76d5bedc2f6212cdcb916e358cc3953626e8c198
Dany <lysakdanil@gmail.com>
2025-04-01 08:14:00 +0300

    Feat: Add budget creation page

---
+++ src/pages/budgets/budget-creation.vue
@@ -0,0 +1,91 @@
+<script setup lang="ts">
+import { ref, computed } from "vue";
+
+import UiButton from "@/components/common/ui-button.vue";
+import DateField from "@/components/fields/date-field.vue";
+import InputField from "@/components/fields/input-field.vue";
+import Checkbox from "@/components/lib/ui/checkbox/Checkbox.vue";
+
+import { createBudget } from "@/api/budgets";
+import { VUE_QUERY_CACHE_KEYS } from "@/common/const";
+import { useMutation, useQueryClient } from "@tanstack/vue-query";
+
+const BUDGET_DEFAULT_VALUES: {
+  name: string | null;
+  categoryName: string | null;
+  startDate?: Date | null;
+  endDate?: Date | null;
+  limitAmount?: number | null;
+  autoInclude?: boolean;
+} = {
+  name: null,
+  categoryName: null,
+  startDate: null,
+  endDate: null,
+  limitAmount: 0,
+  autoInclude: false,
+} as const;
+
+const queryClient = useQueryClient();
+
+const { isPending: isMutating, mutate } = useMutation({
+  mutationFn: createBudget,
+  onSuccess: () => {
+    queryClient.invalidateQueries({ queryKey: VUE_QUERY_CACHE_KEYS.budgetsList });
+  },
+});
+
+const form = ref({ ...BUDGET_DEFAULT_VALUES });
+
+const isDateExist = computed(() => !!form.value.startDate && !!form.value.endDate);
+const isSubmitDisabled = computed(() => isMutating.value || !form.value.name);
+
+const createBudgetItem = async () => {
+  await mutate(form.value);
+};
+</script>
+
+<template>
+  <form class="grid gap-4">
+    <InputField v-model="form.name" label="Budget name" placeholder="Enter the name" />
+    <div class="flex justify-between gap-4">
+      <DateField
+        v-model="form.startDate"
+        :calendar-options="{
+          maxDate: form.endDate,
+        }"
+        label="From date"
+      />
+      <DateField
+        v-model="form.endDate"
+        :calendar-options="{
+          minDate: form.startDate,
+        }"
+        label="To date"
+      />
+    </div>
+
+    <div class="flex gap-2">
+      <label class="cursor-pointer flex gap-2 items-center">
+        <Checkbox
+          :checked="form.autoInclude"
+          :disabled="!isDateExist"
+          @update:checked="form.autoInclude = $event"
+        />
+        Auto include transactions
+      </label>
+    </div>
+
+    <div>
+      <InputField
+        v-model.number="form.limitAmount"
+        label="Budget limit"
+        placeholder="Enter the limit"
+      />
+    </div>
+
+    <div class="mt-4">
+      <UiButton :disabled="isSubmitDisabled" @click="createBudgetItem">Add budget</UiButton>
+    </div>
+  </form>
+</template>
